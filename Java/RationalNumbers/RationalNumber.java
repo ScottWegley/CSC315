51cfa58461f2dcd556b6eeb4d891fa1993b959f9
scottwegley <scottwegley@gmail.com>
2023-08-29 14:27:50 -0700

    blank rational number class with empty interface methods

--- Java/RationalNumbers/RationalNumber.java
+++ Java/RationalNumbers/RationalNumber.java
@@ -1,5 +1,47 @@
 package RationalNumbers;
 
-public class RationalNumber {
+public class RationalNumber implements RationalNumberInterface{
+
+    @Override
+    public int getNumerator() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getDenominator() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public RationalNumberInterface add(RationalNumberInterface rhs) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public RationalNumberInterface sub(RationalNumberInterface rhs) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public RationalNumberInterface mult(RationalNumberInterface rhs) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public RationalNumberInterface div(RationalNumberInterface rhs) throws ArithmeticException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public double sqrt() throws ArithmeticException {
+        // TODO Auto-generated method stub
+        return 0;
+    }
     
 }

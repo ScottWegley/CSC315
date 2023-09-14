f22bbb4fe73444039b20721be71b34bafa178306
Scott Wegley <scbrwegley@protonmail.ch>
2023-09-14 12:41:19 -0700

    arithmetic operations

--- CPP/RationalNumbers/RationalNumber.cpp
+++ CPP/RationalNumbers/RationalNumber.cpp
@@ -22,19 +22,40 @@ RationalNumber::RationalNumber(const RationalNumber &nrn)
 {
 }
 
-RationalNumber::RationalNumber(int n, int d)
-: numerator(n), denominator(d) {
-    if(d == 0){
-        throw std::invalid_argument("The denominator cannot be 0.");
+RationalNumber RationalNumber::add(const RationalNumber &rhs) const
+{
+    return RationalNumber(this->numerator * rhs.getDenominator() + this->denominator * rhs.getNumerator(), this->denominator * rhs.getDenominator());
+}
+
+RationalNumber RationalNumber::sub(const RationalNumber &rhs) const
+{
+    return RationalNumber(this->numerator * rhs.getDenominator() - this->denominator * rhs.getNumerator(), this->denominator * rhs.getDenominator());
+}
+
+RationalNumber RationalNumber::mult(const RationalNumber &rhs) const
+{
+    return RationalNumber(this->numerator * rhs.getNumerator(), this->denominator * rhs.getDenominator());
+}
+
+RationalNumber RationalNumber::div(const RationalNumber &rhs) const
+{
+    if (rhs.getNumerator() == 0)
+        throw "Divide By Zero Exception";
+    return RationalNumber(this->numerator * rhs.getDenominator(), this->denominator * rhs.getNumerator());
+}
+
+double RationalNumber::sqrt() const
+{
+    if (this->numerator < 0)
+    {
+        throw "Cannot square root a negative number";
     }
-    if(d < 0){
-        this->numerator *= -1;
-        this->denominator *= -1;
+    else
+    {
+        return std::sqrt((double)this->numerator / (double)this->denominator);
     }
 }
 
-RationalNumber::RationalNumber(const RationalNumber &nrn)
-: numerator(nrn.numerator), denominator(nrn.denominator) {
 }
 
 RationalNumber RationalNumber::add(const RationalNumber &rhs){

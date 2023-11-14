02b3dc2a7dbcba229d9a4f6efb62e8575640d533
Scott Wegley <scbrwegley@protonmail.ch>
2023-11-14 13:16:50 -0800

    rationalnumberarray constructor finished array initialization, untested

--- CPP/RationalNumberArray/RationalNumberArray.cpp
+++ CPP/RationalNumberArray/RationalNumberArray.cpp
@@ -2,13 +2,33 @@
 
 using namespace std;
 
-RationalNumberArray::RationalNumberArray(int rows, int cols):rows(rows),cols(cols){
-    if(rows <= 0 ){
+RationalNumberArray::RationalNumberArray(int rows, int cols) : rows(rows), cols(cols)
+{
+    if (rows <= 0)
+    {
         throw "Cannot create an array with 0 or negative rows";
     }
-    if(cols <= 0){
+    if (cols <= 0)
+    {
         throw "Cannot create an array with 0 or negative columns";
     }
 
-    
+    this->data = new RationalNumber *[rows];
+
+    for (int i = 0; i < rows; i++)
+    {
+        if (i == 0)
+        {
+            this->data[0] = new RationalNumber[rows * cols];
+        }
+        else
+        {
+            this->data[i] = this->data[i - 1] + cols;
+        }
+        for (int j = 0; j < cols; j++)
+        {
+            this->data[i][j] = RationalNumber(j + i * cols + 1, 1);
+        }
+    }
 }
+

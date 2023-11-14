18fddf2c9e69153e55d53ed2a7d21642f7dfc4bc
Scott Wegley <scbrwegley@protonmail.ch>
2023-11-14 13:13:42 -0800

    constructor for rationalnumberarray only bounds checks the rows and columns

--- CPP/RationalNumberArray/RationalNumberArray.cpp
+++ CPP/RationalNumberArray/RationalNumberArray.cpp
@@ -1,3 +1,14 @@
 #include "RationalNumberArray.h"
 
 using namespace std;
+
+RationalNumberArray::RationalNumberArray(int rows, int cols):rows(rows),cols(cols){
+    if(rows <= 0 ){
+        throw "Cannot create an array with 0 or negative rows";
+    }
+    if(cols <= 0){
+        throw "Cannot create an array with 0 or negative columns";
+    }
+
+    
+}
